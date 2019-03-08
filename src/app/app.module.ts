e1f9e8aa5f0fedcf08812e0c46dfe9369c2996e4
Paweł Twardziak <programmer@it-opes.com>
2019-03-09 00:09:02 +0100

    Setting up base application frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { MatToolbarModule, MatCardModule } from '@angular/material';
+import { registerLocaleData } from '@angular/common';
+import localePl from '@angular/common/locales/pl';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePl, 'pl');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pl' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
