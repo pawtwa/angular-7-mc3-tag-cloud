662c6729b3cbfd9f183a8921fe9988166f0676bd
Paweł Twardziak <programmer@it-opes.com>
2019-03-13 00:35:04 +0100

    Updates

---
+++ assets/task-requirements.ts
@@ -0,0 +1,8 @@
+export default [
+    'Prezentacja różnych wyrazów',
+    'Każdy wyraz ma inną ilość znaków',
+    'Każdy wyraz ma inną wielkość',
+    'Wyrazy wraz ich wielkościami należy pobrać z plik *.json',
+    'Dodatkowy punkt za dodanie obsługi błędów w przypadku braku pliku *.json',
+    'Dodatkowy punkt za zbudowanie interfejsu odpowiedzi z serwera'
+]
